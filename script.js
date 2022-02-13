580eb4f93d1d55ffd24afb3107f13f2ae4f2dffe
OwOreiwa <99535741+OwOreiwa@users.noreply.github.com>
2022-02-13 20:22:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request == "Action") {
-        window.alert('部屋をお掃除するお！');
         for(var i=1; i<5; i++){
             RemoveRoom_Friends();
             RemoveRoom_Chat();
@@ -39,17 +38,41 @@ function RemoveRoom_Friends(){
 function addRoleSettings(){
     'use strict';
     const addBtn = (h, title, func) => {
-        return $("<button>").text(title).click(func).appendTo(h);
+        return $('<button style="width: 55px; margin: 2px;">').text(title).click(func).appendTo(h);
     }
 
     const h = $("<div>").css({
         "text-align": "center",
         "background-color": "darkgray",
-        "border": "3px solid gray",
-        "border-radius": "10px",
-    }).prependTo(".voteAction").append("<h3>配役設定</h3>");
+        "border": "10px double gray",
+        "border-radius": "20px",
+    })
+    $("input[value=村設定変更]").before(h)
+    h.append("<h3>配役設定</h3>");
     h.attr("id", "rolesSetting");
 
+    addBtn(h, "8cfo", () => {
+        $('select[name=job_1]').val(2);     //人狼
+        $('select[name=job_4]').val(1);     //占い師
+        $('select[name=job_3]').val(0);     //狩人
+        $('select[name=job_5]').val(1);     //霊能者
+        $('select[name=job_6]').val(1);     //狂人
+        $('select[name=job_7]').val(0);     //狂信者
+        $('select[name=job_19]').val(0);    //黒猫
+        $('select[name=job_8]').val(0);     //妖狐
+        $('select[name=job_9]').val(0);     //背徳者
+        $('select[name=job_16]').val(0);    //てるてる
+        $('select[name=job_17]').val(0);    //恋人
+        $('select[name=job_10]').val(0);    //猫又
+        $('select[name=job_11]').val(2);    //共有者
+        $('select[name=job_18]').val(0);    //パン屋
+        $('select[name=job_12]').val(0);    //役人
+        $('select[name=job_13]').val(0);    //怪盗
+        $('select[name=job_14]').val(0);    //狼憑き
+        $('select[name=job_15]').val(0);    //ものまね
+        $('input[name=yakukakenashi]').prop("checked", false);   //役欠けなし
+        $('input[value=村設定変更]').trigger("click");
+    });
     addBtn(h, "10a狩", () => {
         $('select[name=job_1]').val(2);     //人狼
         $('select[name=job_4]').val(1);     //占い師
@@ -69,7 +92,8 @@ function addRoleSettings(){
         $('select[name=job_13]').val(0);    //怪盗
         $('select[name=job_14]').val(0);    //狼憑き
         $('select[name=job_15]').val(0);    //ものまね
-        $('select[name=teiin]').val(10).trigger("change");
+        $('input[name=yakukakenashi]').prop("checked", false);   //役欠けなし
+        $('input[value=村設定変更]').trigger("click");
     });
     addBtn(h, "12a猫", () => {
         $('select[name=job_1]').val(3);     //人狼
@@ -90,7 +114,30 @@ function addRoleSettings(){
         $('select[name=job_13]').val(0);    //怪盗
         $('select[name=job_14]').val(0);    //狼憑き
         $('select[name=job_15]').val(0);    //ものまね
-        $('select[name=teiin]').val(12).trigger("change");
+        $('input[name=yakukakenashi]').prop("checked", false);   //役欠けなし
+        $('input[value=村設定変更]').trigger("click");
+    });
+    addBtn(h, "12b", () => {
+        $('select[name=job_1]').val(2);     //人狼
+        $('select[name=job_4]').val(1);     //占い師
+        $('select[name=job_3]').val(1);     //狩人
+        $('select[name=job_5]').val(1);     //霊能者
+        $('select[name=job_6]').val(1);     //狂人
+        $('select[name=job_7]').val(0);     //狂信者
+        $('select[name=job_19]').val(0);    //黒猫
+        $('select[name=job_8]').val(1);     //妖狐
+        $('select[name=job_9]').val(0);     //背徳者
+        $('select[name=job_16]').val(0);    //てるてる
+        $('select[name=job_17]').val(0);    //恋人
+        $('select[name=job_10]').val(0);    //猫又
+        $('select[name=job_11]').val(0);    //共有者
+        $('select[name=job_18]').val(0);    //パン屋
+        $('select[name=job_12]').val(0);    //役人
+        $('select[name=job_13]').val(0);    //怪盗
+        $('select[name=job_14]').val(0);    //狼憑き
+        $('select[name=job_15]').val(0);    //ものまね
+        $('input[name=yakukakenashi]').prop("checked", false);   //役欠けなし
+        $('input[value=村設定変更]').trigger("click");
     });
     addBtn(h, "14d猫", () => {
         $('select[name=job_1]').val(3);     //人狼
@@ -111,6 +158,53 @@ function addRoleSettings(){
         $('select[name=job_13]').val(0);    //怪盗
         $('select[name=job_14]').val(0);    //狼憑き
         $('select[name=job_15]').val(0);    //ものまね
-        $('select[name=teiin]').val(14).trigger("change");
+        $('input[name=yakukakenashi]').prop("checked", false);   //役欠けなし
+        $('input[value=村設定変更]').trigger("click");
     });
+    addBtn(h, "八割", () => {
+        $('select[name=job_1]').val(2);     //人狼
+        $('select[name=job_4]').val(2);     //占い師
+        $('select[name=job_3]').val(0);     //狩人
+        $('select[name=job_5]').val(0);     //霊能者
+        $('select[name=job_6]').val(1);     //狂人
+        $('select[name=job_7]').val(0);     //狂信者
+        $('select[name=job_19]').val(0);    //黒猫
+        $('select[name=job_8]').val(0);     //妖狐
+        $('select[name=job_9]').val(0);     //背徳者
+        $('select[name=job_16]').val(0);    //てるてる
+        $('select[name=job_17]').val(0);    //恋人
+        $('select[name=job_10]').val(0);    //猫又
+        $('select[name=job_11]').val(0);    //共有者
+        $('select[name=job_18]').val(0);    //パン屋
+        $('select[name=job_12]').val(0);    //役人
+        $('select[name=job_13]').val(1);    //怪盗
+        $('select[name=job_14]').val(0);    //狼憑き
+        $('select[name=job_15]').val(0);    //ものまね
+        $('input[name=yakukakenashi]').prop("checked", false);   //役欠けなし
+        $('input[value=村設定変更]').trigger("click");
+    });
+    addBtn(h, "微細", () => {
+        $('select[name=job_1]').val(2);     //人狼
+        $('select[name=job_4]').val(1);     //占い師
+        $('select[name=job_3]').val(1);     //狩人
+        $('select[name=job_5]').val(1);     //霊能者
+        $('select[name=job_6]').val(0);     //狂人
+        $('select[name=job_7]').val(0);     //狂信者
+        $('select[name=job_19]').val(1);    //黒猫
+        $('select[name=job_8]').val(1);     //妖狐
+        $('select[name=job_9]').val(1);     //背徳者
+        $('select[name=job_16]').val(0);    //てるてる
+        $('select[name=job_17]').val(2);    //恋人
+        $('select[name=job_10]').val(1);    //猫又
+        $('select[name=job_11]').val(0);    //共有者
+        $('select[name=job_18]').val(0);    //パン屋
+        $('select[name=job_12]').val(0);    //役人
+        $('select[name=job_13]').val(0);    //怪盗
+        $('select[name=job_14]').val(1);    //狼憑き
+        $('select[name=job_15]').val(0);    //ものまね
+        $('input[name=yakukakenashi]').prop("checked", false);   //役欠けなし
+        $('input[value=村設定変更]').trigger("click");
+    });
+
+    h.append('<h3 style="margin: 10px"></h3>');
 };
